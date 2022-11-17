5e0be3261a51b97df9d6f38e1750bca838717089
Shubham Sakhare <66584634+shubhamcancode@users.noreply.github.com>
2022-11-17 17:13:22 +0530

    problem solved

---
+++ Functions/IsEven.java
@@ -0,0 +1,20 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class IsEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+        System.out.println("The number is = ");
+
+    }
+    public static int Check(int num){
+        if(num % 2 == 0){
+
+        }
+
+        return num;
+    }
+}
