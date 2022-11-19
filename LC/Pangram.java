ee1dc58e11cd65788c53d2799996eafecc50346e
Shubham Sakhare <66584634+shubhamcancode@users.noreply.github.com>
2022-11-20 02:46:06 +0530

    problem solved

---
+++ LC/Pangram.java
@@ -0,0 +1,19 @@
+package LC;
+//ignore package
+// String[] sentence = new String["abcdefghijklmnopqrstuvwxyz"];
+public class Pangram {
+        public boolean checkIfPangram(String sentence) {
+            boolean[] arr = new boolean[26];
+            for(int i=0; i<sentence.length(); i++){
+                arr[sentence.charAt(i) - 97] = true;
+            }
+            for(int i=0; i<26; i++){
+                if(arr[i] == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+}
