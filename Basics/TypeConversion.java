61789e70e950c79ab51ae2101f80fcf479e995eb
Shubham Sakhare <66584634+shubhamcancode@users.noreply.github.com>
2022-12-03 21:56:12 +0530

    Core concepts

---
+++ Basics/TypeConversion.java
@@ -0,0 +1,31 @@
+package Basics;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+//        narrowing conversion
+        float x = 6.24f;
+        int y = (int)x;
+        System.out.println(y);
+
+//        upgrading conversion
+        int a = 22;
+        float b = a;
+
+        float c = 4.45f;
+        /*
+        Upgrading Conversions
+        1. byte -> short, int, long, float, or double
+        2. short -> int, long, float, or double
+        3. char -> int, long, float, or double
+        4. int -> long, float, or double
+        5. long -> float, or double
+        6. float -> double
+        */
+
+
+
+
+
+
+    }
+}
