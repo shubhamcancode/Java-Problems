24d276f3c73929710b28ca0003cc3a7a3faf3262
Shubham Sakhare <66584634+shubhamcancode@users.noreply.github.com>
2022-12-10 01:07:03 +0530

    problem solved

---
+++ LC/LC_SmallestNumber.java
@@ -0,0 +1,28 @@
+package LC;
+
+public class LC_SmallestNumber {
+    //744. Find Smallest Letter Greater Than Target (Leetcode)
+
+        public static void main(String[] args) {
+            char[] letters = {'c', 'f', 'j','x','y'};
+            char target = 'r';
+
+            char ans = smallestLetter(letters, target);
+            System.out.println("Ceiling char = "+ans);
+        }
+        static char smallestLetter(char[] letters, char target){
+            int start = 0;
+            int end = letters.length - 1;
+
+            while(start <= end){
+                int mid = start + (end - start)/2;
+
+                if(target < letters[mid]){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }
+            return letters[start % letters.length];
+        }
+    }
